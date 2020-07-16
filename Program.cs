3835407b53ca9e4a27f5f5eec6cbd6b9014cc4a3
Vlad Prysykar <v.prysykar@multiplex.ua>
2020-07-16 13:33:18 +0300

    working bot git add

---
+++ Program.cs
@@ -0,0 +1,429 @@
+﻿//using System;
+
+//namespace kinodrom_bot
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+
+//            var sessions = new database().GetSessions();
+//            foreach(var kvp in sessions)
+//            {
+//                Console.WriteLine($"{kvp.Key} {kvp.Value}");
+//                var info = new database().GetKinodromOrders(kvp.Value);
+//                info = new database().GetKinodromOrders_seats(info);
+//                foreach(var i in info)
+//                {
+//                    Console.WriteLine($"{i.TransactionNumber}-{i.BookingNumber}-{i.Email}");
+//                    foreach(var items in i.items)
+//                    {
+//                        Console.WriteLine($"\t\t{items.name} x{items.qty}");
+//                    }
+//                    foreach (var t in i.tickets)
+//                    {
+//                        Console.WriteLine($"\t\tряд:{t.RowId}  місце:{t.SeatId}");
+//                    }
+//                }
+//            }
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
+
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace kinodrom_bot
+{
+    public static class Program
+    {
+        private static TelegramBotClient Bot;
+        private static database db;
+
+        static async Task Main()
+        {
+
+            IConfiguration Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .Build();
+
+
+            db = new database(Configuration.GetSection("db_connectionstring").Value);
+            string BotToken = Configuration.GetSection("bot_token").Value;
+
+            Bot = new TelegramBotClient(BotToken);
+            //#if USE_PROXY
+            //            var Proxy = new WebProxy(Configuration.Proxy.Host, Configuration.Proxy.Port) { UseDefaultCredentials = true };
+            //            Bot = new TelegramBotClient(Configuration.BotToken, webProxy: Proxy);
+            //#else
+            //            Bot = new TelegramBotClient(BotToken);
+            //#endif
+
+            var me = await Bot.GetMeAsync();
+            Console.Title = me.Username;
+
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnInlineQuery += BotOnInlineQueryReceived;
+            Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.WriteLine($"Start listening for @{me.Username}");
+
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+
+        static async Task BotOnException(long chat_id, string exceptionMsg)
+        {
+            await Bot.SendTextMessageAsync(
+                    chatId: chat_id,
+                    text: exceptionMsg
+                );
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.Text)
+                return;
+
+            switch (message.Text.Split(' ').First())
+            {
+
+                case "/kinodrom@DevService_bot":
+                case "/sessions":
+                case "/sessions@DevService_bot":
+                    await SendSessionsList(message);
+                    break;              
+                //// Send inline keyboard
+                //case "/inline":
+                //    await SendInlineKeyboard(message);
+                //    break;
+
+                //// send custom keyboard
+                //case "/keyboard":
+                //    await SendReplyKeyboard(message);
+                //    break;
+
+                //// send a photo
+                //case "/photo":
+                //    await SendDocument(message);
+                //    break;
+
+                //// request location or contact
+                //case "/request":
+                //    await RequestContactAndLocation(message);
+                //    break;
+
+                default:
+                    await mxUsage(message);
+                    break;
+            }
+
+            async Task SendSessionsList(Message msg)
+            {
+                List<database.SessionInfo> sessions_dc = new List<database.SessionInfo>();
+                try
+                {
+                    sessions_dc = db.GetSessions();
+                }
+                catch (Exception e)
+                {
+                    await BotOnException(message.Chat.Id, $"db.GetSessions exception:{e.Message}");
+                    return;
+                }
+                if (sessions_dc.Count == 0)
+                {
+                    await Bot.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: "Сеанси не знайдено"
+                    );
+
+                    return;
+                }
+
+
+                var rows = new List<List<InlineKeyboardButton>>();
+                foreach (var se in sessions_dc)
+                {
+                    List<InlineKeyboardButton> button = new List<InlineKeyboardButton>();
+                    button.Add(InlineKeyboardButton.WithCallbackData($"{se.showtime.ToString("HH:mm  dd.MM ")}  {se.FilmTitle}", $"session_id-{se.sessionId}"));
+                    rows.Add(button);
+                }
+
+                var inlineKeyboard = new InlineKeyboardMarkup(rows.ToArray().ToArray());
+
+                await Bot.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Оберіть сеанс:",
+                    replyMarkup: inlineKeyboard
+                );
+            }
+
+            // Send inline keyboard
+            // You can process responses in BotOnCallbackQueryReceived handler
+            async Task SendInlineKeyboard(Message msg)
+            {
+                await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+                // Simulate longer running task
+                await Task.Delay(500);
+
+                var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                {
+                    // first row
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData("1.1", "11"),
+                        InlineKeyboardButton.WithCallbackData("1.2", "12"),
+                    },
+                    // second row
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData("2.1", "21"),
+                        InlineKeyboardButton.WithCallbackData("2.2", "22"),
+                    }
+                });
+                await Bot.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Choose",
+                    replyMarkup: inlineKeyboard
+                );
+            }
+
+            async Task SendReplyKeyboard(Message msg)
+            {
+                var replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                    new KeyboardButton[][]
+                    {
+                        new KeyboardButton[] { "1.1", "1.2" },
+                        new KeyboardButton[] { "2.1", "2.2" },
+                    },
+                    resizeKeyboard: true
+                );
+
+                await Bot.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Choose",
+                    replyMarkup: replyKeyboardMarkup
+
+                );
+            }
+
+            async Task SendDocument(Message msg)
+            {
+                await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+
+                const string filePath = @"Files/tux.png";
+                var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
+                await Bot.SendPhotoAsync(
+                    chatId: message.Chat.Id,
+                    photo: new InputOnlineFile(fileStream, fileName),
+                    caption: "Nice Picture"
+                );
+            }
+
+            async Task RequestContactAndLocation(Message msg)
+            {
+                var RequestReplyKeyboard = new ReplyKeyboardMarkup(new[]
+                {
+                    KeyboardButton.WithRequestLocation("Location"),
+                    KeyboardButton.WithRequestContact("Contact"),
+                });
+                await Bot.SendTextMessageAsync(
+                    chatId: msg.Chat.Id,
+                    text: "Who or Where are you?",
+                    replyMarkup: RequestReplyKeyboard
+                );
+            }
+
+            async Task Usage(Message msg)
+            {
+                const string usage = "Usage:\n" +
+                                        "/inline   - send inline keyboard\n" +
+                                        "/keyboard - send custom keyboard\n" +
+                                        "/photo    - send a photo\n" +
+                                        "/request  - request location or contact";
+                await Bot.SendTextMessageAsync(
+                    chatId: msg.Chat.Id,
+                    text: usage,
+                    replyMarkup: new ReplyKeyboardRemove()
+                );
+            }
+
+            async Task mxUsage(Message msg)
+            {
+                const string usage = "Usage:\n" +
+                                        "/sessions - show todays sessions list";
+                await Bot.SendTextMessageAsync(
+                    chatId: msg.Chat.Id,
+                    text: usage,
+                    replyMarkup: new ReplyKeyboardRemove()
+                );
+            }
+        }
+
+        // Process Inline Keyboard callback data
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+
+            if (callbackQuery.Data.Contains("session_id-"))
+            {
+                List<database.singleOrderInfo> order_info = new List<database.singleOrderInfo>();
+                var incoming_msg = callbackQuery.Data;
+                var session_id = Convert.ToInt32(callbackQuery.Data.Substring(incoming_msg.IndexOf('-') + 1));
+                try
+                {
+                    order_info = db.GetKinodromOrders(session_id);
+                }
+                catch (Exception e)
+                {
+                    await BotOnException(callbackQuery.Message.Chat.Id, $"db.GetKinodromOrders exception:{e.Message}");
+                    return;
+                }
+                if (order_info.Count > 0)
+                {
+                    try
+                    {
+                        order_info = db.GetKinodromOrders_seats(order_info);
+                    }
+                    catch (Exception e)
+                    {
+                        await BotOnException(callbackQuery.Message.Chat.Id, $"db.GetKinodromOrders_seats exception:{e.Message}");
+                        return;
+                    }
+
+
+                    var message = MessageTemplateFormat(order_info);
+                    await Bot.SendTextMessageAsync(
+                             chatId: callbackQuery.Message.Chat.Id,
+                            text: message,
+                            parseMode: ParseMode.Html
+                        );
+                }
+                else
+                {
+                    await Bot.SendTextMessageAsync(
+                         chatId: callbackQuery.Message.Chat.Id,
+                        text: "Замовлення відсутні"
+                    );
+                }
+                return;
+            }
+
+            await Bot.AnswerCallbackQueryAsync(
+                callbackQueryId: callbackQuery.Id,
+                text: $"Received {callbackQuery.Data}"
+            );
+
+            await Bot.SendTextMessageAsync(
+                chatId: callbackQuery.Message.Chat.Id,
+                text: $"Received {callbackQuery.Data}"
+            );
+        }
+
+
+
+        #region Inline Mode
+        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+        {
+            Console.WriteLine($"Received inline query from: {inlineQueryEventArgs.InlineQuery.From.Id}");
+
+            InlineQueryResultBase[] results = {
+                // displayed result
+                new InlineQueryResultArticle(
+                    id: "3",
+                    title: "TgBots",
+                    inputMessageContent: new InputTextMessageContent(
+                        "hello"
+                    )
+                )
+            };
+            await Bot.AnswerInlineQueryAsync(
+                inlineQueryId: inlineQueryEventArgs.InlineQuery.Id,
+                results: results,
+                isPersonal: true,
+                cacheTime: 0
+            );
+        }
+
+        private static void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+        {
+            Console.WriteLine($"Received inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
+        }
+
+        #endregion
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message
+            );
+        }
+
+        private static string MessageTemplateFormat(List<database.singleOrderInfo> order_info)
+        {
+
+            //        Cеанс: 10:05
+            //--------------------------------------
+            //Имейл: antonio.onischenko @gmail.com
+            // Номер телефона: 380638427889
+            //Содержимое заказа:
+            //Акція Попкорн сир 3,0л + Пепсі 0,8л(розлив)
+            //Кол - во товаров: 1
+            //Сумма заказа: 85грн.
+            //--------------------------------------
+            string divider = "--------------------------------------";
+            string template = "Номер замовлення: {bnum}\nТелефон: {phone}\nEmail: {email}\n{items}{tickets}Вартість замовлення: {price}грн\n{divider}\n";
+            string ResultMessage = $"Сеанс: {order_info[0].ShowTime.ToString("dd.MM HH:mm")}   {order_info[0].FilmTitle}\n{divider}\n".Replace("{divider}", divider);
+            ResultMessage = ResultMessage.Replace("{session_time}", "Время сеанса");
+            foreach (var x in order_info)
+            {
+                string dat_message = template;
+                dat_message = dat_message.Replace("{divider}", divider);
+                dat_message = dat_message.Replace("{bnum}", x.BookingNumber.ToString());
+                dat_message = dat_message.Replace("{phone}", x.Phone.ToString());
+                dat_message = dat_message.Replace("{email}", x.Email.ToString());
+                dat_message = dat_message.Replace("{price}", x.total_price.ToString("N1"));
+                var items = "<i>Перелік продукції:</i>\n";
+                foreach (var i in x.items)
+                {
+                    items += $"{i.qty}шт <b>{i.name}</b>\n";
+                }
+                var tickets = "<i>Інформація про місця:</i>\n";
+                foreach (var t in x.tickets)
+                {
+                    tickets += $"Ряд:{t.RowId} місце:{t.SeatId}\n";
+                }
+                dat_message = dat_message.Replace("{items}", items);
+                dat_message = dat_message.Replace("{tickets}", tickets);
+                ResultMessage += dat_message;
+            }
+            return ResultMessage;
+        }
+    }
+}
---
+++ database.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace kinodrom_bot
+{
+    public class database
+    {
+        string cs = "";// "Data Source=SQL01AO\\ROV06VCDB02;Initial Catalog=VISTA;Integrated Security=false;User ID=VistaApp; Password=pu3HNrAxZSjeKXYA";
+
+        public database(string _cs)
+        {
+            cs = _cs;
+        }
+        public class SessionInfo
+        {
+            public DateTime showtime;
+            public int sessionId;
+            public string FilmTitle;
+        }
+
+        public List<SessionInfo> GetSessions()
+        {
+            List<SessionInfo> session_pairs = new List<SessionInfo>();
+
+            var sql = @"SELECT SN.Session_dtmShowing, SN.Session_lngSessionId, FM.Film_strTitle   
+                    FROM tblSession SN 
+                    JOIN tblFilm FM ON SN.Film_strCode = FM.Film_strCode
+                    WHERE Session_strAttributes like '%CAR%' 
+                    AND @timeFrom < Session_dtmShowing
+                    AND @timeTo > Session_dtmShowing
+                    AND SN.Session_strStatus = 'O'";
+
+            //'2020-07-15 06:00:00.000'
+
+            //var configuration = new ConfigurationBuilder()
+            //.SetBasePath(Directory.GetCurrentDirectory())
+            //.AddJsonFile("config.json", false)
+            //.Build();
+
+            //var connectionString = configuration.GetSection("connectionString").Value;
+
+            if (string.IsNullOrEmpty(cs))
+                throw new ArgumentException("No connection string in config.json");
+
+            using (var conn = new SqlConnection(cs))
+            {
+                conn.Open();
+
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("timeFrom",  DateTime.Today.AddHours(6).ToString("yyyy-MM-dd HH:mm:ss"));
+                    cmd.Parameters.AddWithValue("timeTo", DateTime.Today.AddDays(1).AddHours(6).ToString("yyyy-MM-dd HH:mm:ss"));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            var showTime = reader.GetDateTime(0);
+                            var SessionId = reader.GetInt32(1);
+                            var filmTitle = reader.GetString(2);
+                            session_pairs.Add(new SessionInfo { sessionId = SessionId, showtime = showTime, FilmTitle = filmTitle });
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            return session_pairs;
+
+        }
+
+        public List<singleOrderInfo> GetKinodromOrders(int session_id)
+        {
+            List<singleOrderInfo> result = new List<singleOrderInfo>();
+
+            var sql = @"SELECT BH.BookingH_strEmail, BH.BookingH_strPhone, BH.BookingH_intNextBookingNo, TI.TransI_lgnNumber, COUNT(TI.Item_strItemId), ITM.Item_strItemDescription 
+                        FROM tblTrans_Inventory TI
+                        JOIN tblBooking_Header BH ON TI.TransI_lgnNumber = BH.TransC_lgnNumber
+                        JOIN tblItem  ITM ON ITM.Item_strItemId = TI.Item_strItemId
+                        WHERE TransI_lgnNumber IN (SELECT TransC_lgnNumber FROM tblBooking_Header BH WHERE BH.BookingH_intNextBookingNo IN (
+                            SELECT BD.BookingD_intNextBookingNo FROM tblBooking_Detail BD WHERE BD.Session_lngSessionId = @1 )) 
+                        AND BH.BookingH_strStatus != 'C'
+                        GROUP BY BookingH_strEmail, BookingH_strPhone, BH.BookingH_intNextBookingNo, TI.TransI_lgnNumber,ITM.Item_strItemDescription ";
+
+            //var configuration = new ConfigurationBuilder()
+            //.SetBasePath(Directory.GetCurrentDirectory())
+            //.AddJsonFile("config.json", false)
+            //.Build();
+
+            //var connectionString = configuration.GetSection("connectionString").Value;
+
+            if (string.IsNullOrEmpty(cs))
+                throw new ArgumentException("No connection string in config.json");
+
+            using (var conn = new SqlConnection(cs))
+            {
+                conn.Open();
+
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("1", session_id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            singleOrderInfo ss = new singleOrderInfo();
+                            ss.Email = reader.GetString(0);
+                            ss.Phone = reader.GetString(1);
+                            ss.BookingNumber = reader.GetInt32(2);
+                            ss.TransactionNumber = reader.GetInt32(3);
+                            var qty = reader.GetInt32(4);
+                            var i_name = reader.GetString(5);
+
+                            var lookup_tr = result.Find(x => x.TransactionNumber == ss.TransactionNumber);
+                            if (lookup_tr == null)
+                            {
+                                ss.items.Add(new item_det { qty = qty, name = i_name });
+                                result.Add(ss);
+                            }
+                            else
+                            {
+                                lookup_tr.items.Add(new item_det { qty = qty, name = i_name });
+                            }
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            return result;
+        }
+
+        public List<singleOrderInfo> GetKinodromOrders_seats(List<singleOrderInfo> data)
+        {
+            List<int> bnum = new List<int>();
+            data.ForEach(d => bnum.Add(d.BookingNumber));
+            if (data.Count == 0)
+            {
+                return data;
+            }
+            var sql = @"SELECT BD.BookingD_intNextBookingNo, ScreenD_strPhyRowId, ScreenD_strSeatId, TC.TransC_curValue, SN.Session_dtmRealShow, FM.Film_strTitle   
+                    FROM tblBooking_Detail BD
+                    JOIN tblBooking_Header BH on BD.BookingD_intNextBookingNo = BH.BookingH_intNextBookingNo
+                    JOIN tblTrans_Cash TC on BH.TransC_lgnNumber = TC.TransC_lgnNumber
+                    JOIN tblSession SN ON BD.Session_lngSessionId = SN.Session_lngSessionId
+                    JOIN tblFilm FM ON SN.Film_strCode =  FM.Film_strCode 
+                    WHERE BD.BookingD_intNextBookingNo IN (" + string.Join(", ", bnum.ToArray()) + ")";
+
+            if (string.IsNullOrEmpty(cs))
+                throw new ArgumentException("No connection string in config.json");
+
+            using (var conn = new SqlConnection(cs))
+            {
+                conn.Open();
+
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    //cmd.Parameters.AddWithValue("1", string.Join(", ", bnum.ToArray()));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            var booking_num = reader.GetInt32(0);
+                            var row = Convert.ToInt32(reader.GetString(1));
+                            var seat = Convert.ToInt32(reader.GetString(2));
+                            var price =  reader.GetDecimal(3);
+                            var showtime = reader.GetDateTime(4);
+                            var fTitle = reader.GetString(5);
+
+                            var lookup_tr = data.Find(x => x.BookingNumber == booking_num);
+                            lookup_tr.tickets.Add(new tic_det { RowId = row, SeatId = seat });
+                            lookup_tr.total_price = price;
+                            lookup_tr.ShowTime = showtime;
+                            lookup_tr.FilmTitle = fTitle;
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            return data;
+        }
+
+        public class singleOrderInfo
+        {
+            public string Email;
+            public string Phone;
+            public int BookingNumber;
+            public int TransactionNumber;
+            public List<item_det> items = new List<item_det>();
+            public List<tic_det> tickets = new List<tic_det>();
+            public decimal total_price;
+            public DateTime ShowTime;
+            public string FilmTitle;
+        }
+        public class item_det
+        {
+            public int qty;
+            public string name;
+        }
+        public class tic_det
+        {
+            public int RowId;
+            public int SeatId;
+        }
+    }
+}
